95e5db01339be44ea092b9800039eec71a79b325
Mike McDonald <mikemcdonaldcs@gmail.com>
2025-06-20 16:44:10 -0400

    refactor: move snippet list into separate SnippetList component

---
+++ src/features/SnippetList.jsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import SnippetEditRow from './SnippetEditRow';
+
+function SnippetList({
+  snippets,
+  editId,
+  editFields,
+  setEditId,
+  setEditFields,
+  onSaveEdit,
+  onCancelEdit,
+  onComplete,
+  isSaving,
+}) {
+  return (
+    <ul style={{ listStyleType: 'none', paddingLeft: 0 }}>
+      {snippets
+        .filter((snippet) => !snippet.isCompleted)
+        .map((snippet) => (
+          <li key={snippet.id}>
+            {editId === snippet.id ? (
+              <SnippetEditRow
+                editFields={editFields}
+                setEditFields={setEditFields}
+                onSave={() => onSaveEdit(snippet.id)}
+                onCancel={() => onCancelEdit(null)}
+                isSaving={isSaving}
+              />
+            ) : (
+              <>
+                <input
+                  type="checkbox"
+                  className="checkboxItem"
+                  checked={false}
+                  onChange={() => onComplete(snippet.id)}
+                  disabled={isSaving}
+                />
+                {snippet.practiceType && (
+                  <span> {snippet.practiceType} | </span>
+                )}
+                {snippet.goal}
+                {snippet.metronome && <span> | {snippet.metronome} BPM</span>}
+                {snippet.timeSpent && (
+                  <span> | {snippet.timeSpent} Minutes</span>
+                )}
+                <button
+                  onClick={() => {
+                    setEditId(snippet.id);
+                    setEditFields({
+                      practiceType: snippet.practiceType || '',
+                      goal: snippet.goal || '',
+                      metronome: snippet.metronome || '',
+                      timeSpent: snippet.timeSpent || '',
+                    });
+                  }}
+                  className="editButton"
+                >
+                  Edit
+                </button>
+              </>
+            )}
+          </li>
+        ))}
+    </ul>
+  );
+}
+
+export default SnippetList;
